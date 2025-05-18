33e0185099da3c0726b7965eee444bae2be9c510
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-05-18 14:40:20 +0600

    change onChange to onSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import React, { use, useRef, useState } from "react";
-import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router";
 import { Context } from "./providers/Context";
 
 function App() {
   const navigate = useNavigate();
-  const { formData, setFormData } = use(Context);
+  const { setFormData } = use(Context);
 
-  const receiptRef = useRef();
-
-  const handleChange = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const form = e.target;
+
+    const data = {
+      reservationNo: form.reservationNo.value,
+      roomNo: form.roomNo.value,
+      nationality: form.nationality.value,
+      guestNo: form.guestNo.value,
+      contact: form.contact.value,
+      arrivalDate: form.arrivalDate.value,
+      departureDate: form.departureDate.value,
+      totalPrice: form.totalPrice.value,
+      paymentStatus: form.paymentStatus.value,
+    };
+
+    setFormData(data);
+    navigate("/template");
   };
 
   return (
@@ -26,65 +39,59 @@ function App() {
         </p>
 
         {/* form */}
-        <form className="flex flex-col items-center gap-2 mt-8">
+        <form
+          className="flex flex-col items-center gap-2 mt-8"
+          onSubmit={handleSubmit}
+        >
           <input
             className="px-2 py-1 bg-gray-200 rounded text-center"
             name="reservationNo"
-            onChange={handleChange}
             placeholder="Reservation No"
           />
           <input
             className="px-2 py-1 bg-gray-200 rounded text-center"
             name="roomNo"
-            onChange={handleChange}
             placeholder="Room No"
           />
           <input
             className="px-2 py-1 bg-gray-200 rounded text-center"
             name="nationality"
-            onChange={handleChange}
             placeholder="Nationality"
           />
           <input
             className="px-2 py-1 bg-gray-200 rounded text-center"
             name="guestNo"
-            onChange={handleChange}
             placeholder="No. of Guests"
           />
           <input
             className="px-2 py-1 bg-gray-200 rounded text-center"
             name="contact"
-            onChange={handleChange}
             placeholder="Contact"
           />
           <input
             className="px-2 py-1 bg-gray-200 rounded text-center"
             name="arrivalDate"
-            onChange={handleChange}
             placeholder="Date of Arrival"
           />
           <input
             className="px-2 py-1 bg-gray-200 rounded text-center"
             name="departureDate"
-            onChange={handleChange}
             placeholder="Date of Departure"
           />
           <input
             className="px-2 py-1 bg-gray-200 rounded text-center"
             name="totalPrice"
-            onChange={handleChange}
             placeholder="Total Price"
           />
           <input
             className="px-2 py-1 bg-gray-200 rounded text-center"
             name="paymentStatus"
-            onChange={handleChange}
             placeholder="Payment Status"
           />
+
           <button
             className="px-2 py-1 bg-purple-900 text-white cursor-pointer rounded text-center mt-2"
-            type="button"
-            onClick={() => navigate("/template")}
+            type="submit"
           >
             Generate Receipt
           </button>
